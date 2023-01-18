e6d617c5302bb55b7dd91e14268a37b658a12949
Jay M <jaymansmann@yahoo.com>
2023-01-18 15:21:38 -0500

    Fixing 'app icon' styling

--- src/Games/GameMenuItem/GameMenuItem.js
+++ src/Games/GameMenuItem/GameMenuItem.js
@@ -5,7 +5,7 @@ import "./GameMenuItem.css";
 export default function GameMenuItem({game}) {
     return (
         <Link to={`/games/${game.name}`} className="game-menu__item">
-          <div className="game__icon">{game.image ? (<img className="game__icon-image" src={game.image} alt={`logo for ${game.name}`}/>) : Icons.GAME}</div>
+          <div className="game__icon">{game.image ? (<img height="28" width="28" className="game__icon-image" src={game.image} alt={`logo for ${game.name}`}/>) : Icons.GAME}</div>
           <div className="game__name">{game.name}</div>
         </Link>
       );
